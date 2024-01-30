e8a127a02a629d2f0dcdad488344d12e9c15019a
shubham kumar <krsubam4u@gmail.com>
2024-01-30 20:23:59 +0530

    image modified and data inserted

---
+++ main.js
@@ -0,0 +1,18 @@
+1. what are the various barriers to effective communication? sujjest suitable massures to overcome these barriers.
+
+2. what is the role of interpersonal skills?
+
+3. write short notes on:
+   1. encoding
+   2. decoding
+   3. feedback
+
+4. write a details note on group discussion.
+
+5. grapevine communication explain.
+
+6. do`s and don`ts of group discussion
+
+7. discuss types of interview.
+
+8. difference between cvv and resume.
